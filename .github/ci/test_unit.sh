b14fa8c38de85c2ff5627aceafae885ae36b075e
Alvaro Santos Andres <alvsanand@gmail.com>
2026-04-05 16:31:50 +0200

    fix

--- .github/ci/test_unit.sh
+++ .github/ci/test_unit.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 CURRENT_DIR="$(dirname "$(realpath "$0")")"
 
-helm plugin install --verify=false https://github.com/helm-unittest/helm-unittest.git || true
+helm plugin install https://github.com/helm-unittest/helm-unittest.git || true
 
 helm unittest spark-apps
